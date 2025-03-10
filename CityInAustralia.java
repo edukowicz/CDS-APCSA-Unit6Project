aa72a9c87e5a0576ba1edfedbd717eace4d5dcc0
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-03-10 15:41:13 +0000

    message

--- CityInAustralia.java
+++ CityInAustralia.java
@@ -1,5 +1,25 @@
 class CityInAustralia {
 	
+    private String name;
+    private int population;
+    private String[] hospitals;
+
+    public CityInAustralia(String n, int p, String[] h){
+        name = n;
+        population = p;
+        hospitals = h;
+    }
+
+    public String toString(){
+        String message = "City: " + name + "\n";
+        message += "Population: " + population + "\n";
+        message += "List of hospitals: ";
+        for(int i =0; i < hospitals.length; i++){
+            message += hospitals[i] + ", ";
+        }
+        message+= "\n";
+        return message;
+    }
     
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 // Hospital Locator Starter       A+CR
 class Main {
   public static void main(String[] args) {
-    
-		
+
+    String[] aliceSpringsHospitals = {"Alice Springs Hospital", "Alice Springs Private Hospital", "The Centre for Remote Health"};
+    CityInAustralia aliceSprings = new CityInAustralia("Alice Springs", 33000, aliceSpringsHospitals);
+    System.out.println(aliceSprings);
   }
 }
